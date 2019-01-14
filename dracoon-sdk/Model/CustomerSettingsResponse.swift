84779985cdb92da10a6f241013bed19a7de6fca8
Mathias Schreiner <m.schreiner@dracoon.com>
2019-01-14 17:54:31 +0100

    Fixed models

--- dracoon-sdk/Model/CustomerSettingsResponse.swift
+++ dracoon-sdk/Model/CustomerSettingsResponse.swift
@@ -10,11 +10,11 @@ import Foundation
 public struct CustomerSettingsResponse: Codable {
     
     /** the name of the container in which all user’s home rooms are located */
-    public var homeRoomParentName: String
+    public var homeRoomParentName: String?
     /** refers to the quota of each single user’s home room. “0” represents no quota */
-    public var homeRoomQuota: Int64
+    public var homeRoomQuota: Int64?
     /** if set to true, every user with an Active Directory account gets a personal homeroom. Once activated, this cannot be deactivated **/
-    public var homeRoomsActive: Bool?
+    public var homeRoomsActive: Bool
     /** is the id of the parent of all homerooms**/
     public var homeRoomParentId: Int64?
 }
