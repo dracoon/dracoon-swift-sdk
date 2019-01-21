6282e8af56ea97ce1f95594c8bd0ab2a46ad8bd7
Mathias Schreiner <m.schreiner@dracoon.com>
2019-01-21 14:14:15 +0100

    Updated api error description

--- dracoon-sdk/Internal/DracoonError.swift
+++ dracoon-sdk/Internal/DracoonError.swift
@@ -41,7 +41,7 @@ extension DracoonError {
     public var errorDescription: String? {
         switch self {
         case .api(let error):
-            return error.debugDescription
+            return error?.debugInfo ?? error.debugDescription
         case .account(let error):
             return error.localizedDescription
         case .nodes(error: let error):
