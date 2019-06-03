0a1b0928c5d218d1d90a93c25b4d2a81055c7040
Mathias Schreiner <m.schreiner@dracoon.com>
2019-06-03 16:54:29 +0200

    Implemented /account/user/avatar API

---
+++ dracoon-sdk/Model/Avatar.swift
@@ -0,0 +1,19 @@
+//
+//  Avatar.swift
+//  dracoon-sdk
+//
+//  Copyright © 2019 Dracoon. All rights reserved.
+//
+
+import Foundation
+
+/// - Tag: Avatar
+public struct Avatar: Codable {
+    
+    /** Download URI of avatar */
+    public var avatarUri: String
+    /** Unique avatar identifier */
+    public var avatarUuid: String
+    /** Determines whether user updated the avatar with own image */
+    public var isCustomAvatar: Bool
+}
