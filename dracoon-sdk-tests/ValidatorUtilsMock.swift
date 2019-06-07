a5e18630f6aa5ef03356b60856272b113740e4ef
Mathias Schreiner <m.schreiner@dracoon.com>
2019-06-07 15:55:31 +0200

    Updated example dependency

---
+++ dracoon-sdk-tests/ValidatorUtilsMock.swift
@@ -0,0 +1,9 @@
+//
+//  ValidatorUtilsMock.swift
+//  dracoon-sdk-tests
+//
+//  Created by Mathias Schreiner on 06.06.19.
+//  Copyright © 2019 Dracoon. All rights reserved.
+//
+
+import Foundation
