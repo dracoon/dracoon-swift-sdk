f38273c4d6be2404daac649f6b461fa8a1ef8776
Mathias Schreiner <m.schreiner@dracoon.com>
2019-06-14 12:24:50 +0200

    Removed test mock

--- dracoon-sdk-tests/ValidatorUtilsMock.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ValidatorUtilsMock.swift
-//  dracoon-sdk-tests
-//
-//  Created by Mathias Schreiner on 06.06.19.
-//  Copyright © 2019 Dracoon. All rights reserved.
-//
-
-import Foundation
