cdceb26ff4473a79c28296d1f23f8360772347fe
Mathias Schreiner <m.schreiner@dracoon.com>
2019-07-30 17:19:31 +0200

    Added remaining validation ApiErrorCode tests

--- dracoon-sdk-tests/DracoonApiCodeTests.swift
+++ dracoon-sdk-tests/DracoonApiCodeTests.swift
@@ -107,5 +107,135 @@ class DracoonApiCodeTests: XCTestCase {
         
         XCTAssert(DracoonApiCode.VALIDATION_INVALID_OFFSET_OR_LIMIT.rawValue == 3008)
         XCTAssertEqual(DracoonApiCode.VALIDATION_INVALID_OFFSET_OR_LIMIT.description, "Invalid offset or limit.")
+        
+        // Nodes
+        
+        XCTAssert(DracoonApiCode.VALIDATION_FILE_CANNOT_BE_TARGET.rawValue == 3100)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_FILE_CANNOT_BE_TARGET.description, "File cannot be target of a copy or move operation.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_BAD_FILE_NAME.rawValue == 3101)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_BAD_FILE_NAME.description, "Bad file name.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_EXPIRATION_DATE_IN_PAST.rawValue == 3102)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_EXPIRATION_DATE_IN_PAST.description, "Expiration date is in the past.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_EXPIRATION_DATE_TOO_LATE.rawValue == 3103)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_EXPIRATION_DATE_TOO_LATE.description, "The year is too far in the future. Max year is limited to 9999.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_NODE_ALREADY_EXISTS.rawValue == 3104)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_NODE_ALREADY_EXISTS.description, "Node exists already.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_ALREADY_EXISTS.rawValue == 3105)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_ALREADY_EXISTS.description, "A room with the same name already exists.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_FOLDER_ALREADY_EXISTS.rawValue == 3106)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_FOLDER_ALREADY_EXISTS.description, "A folder with the same name already exists.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_FILE_ALREADY_EXISTS.rawValue == 3107)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_FILE_ALREADY_EXISTS.description, "A file with the same name already exists.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_NODES_NOT_IN_SAME_PARENT.rawValue == 3108)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_NODES_NOT_IN_SAME_PARENT.description, "Folders/files must be in same parent.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_CANNOT_COPY_NODE_TO_OWN_PLACE_WITHOUT_RENAME.rawValue == 3109)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_CANNOT_COPY_NODE_TO_OWN_PLACE_WITHOUT_RENAME.description, "Node cannot be copied to its own place without renaming.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_CANNOT_MOVE_NODE_TO_OWN_PLACE.rawValue == 3110)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_CANNOT_MOVE_NODE_TO_OWN_PLACE.description, "Node cannot be moved to its own place.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_FOLDER_CAN_NOT_BE_OVERWRITTEN.rawValue == 3111)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_FOLDER_CAN_NOT_BE_OVERWRITTEN.description, "A room or folder cannot be overwritten.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_CANNOT_COPY_TO_CHILD.rawValue == 3112)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_CANNOT_COPY_TO_CHILD.description, "Node cannot be copied into its child node.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_CANNOT_MOVE_TO_CHILD.rawValue == 3113)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_CANNOT_MOVE_TO_CHILD.description, "Node cannot be moved into its child node.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_CANNOT_COPY_ROOM.rawValue == 3114)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_CANNOT_COPY_ROOM.description, "Rooms cannot be copied.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_CANNOT_MOVE_ROOM.rawValue == 3115)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_CANNOT_MOVE_ROOM.description, "Rooms cannot be moved.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_PATH_TOO_LONG.rawValue == 3116)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_PATH_TOO_LONG.description, "Path is too long.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_NODE_NO_FAVORITE.rawValue == 3117)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_NODE_NO_FAVORITE.description, "Node is not marked as favorite.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_NOT_ENCRYPTED.rawValue == 3118)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_NOT_ENCRYPTED.description, "Room is not encrypted.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_SOURCE_ROOM_ENCRYPTED.rawValue == 3119)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_SOURCE_ROOM_ENCRYPTED.description, "Encrypted files cannot be copied or moved to an unencrypted room.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_TARGET_ROOM_ENCRYPTED.rawValue == 3120)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_TARGET_ROOM_ENCRYPTED.description, "Not encrypted files cannot be copied or moved to an encrypted room.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_ENCRYPTED.rawValue == 3121)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_ENCRYPTED.description, "Room is encrypted.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_CANNOT_BE_UNENCRYPTED_WITH_FILES.rawValue == 3122)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_CANNOT_BE_UNENCRYPTED_WITH_FILES.description, "Room with files cannot be unencrypted.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_CANNOT_BE_ENCRYPTED_WITH_FILES.rawValue == 3123)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_CANNOT_BE_ENCRYPTED_WITH_FILES.description, "Room with files cannot be encrypted.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_ALREADY_HAS_RESCUE_KEY.rawValue == 3124)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_ALREADY_HAS_RESCUE_KEY.description, "Only one room emergency password (rescue key) is allowed.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_CANNOT_SET_ENCRYPTED_WITH_RECYCLEBIN.rawValue == 3125)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_CANNOT_SET_ENCRYPTED_WITH_RECYCLEBIN.description, "Room with not empty recycle bin cannot be set encrypted.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ROOM_CANNOT_SET_UNENCRYPTED_WITH_RECYCLEBIN.rawValue == 3126)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ROOM_CANNOT_SET_UNENCRYPTED_WITH_RECYCLEBIN .description, "Room with not empty recycle bin cannot be set unencrypted.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_ENCRYPTED_FILE_CAN_ONLY_BE_RESTORED_IN_ORIGINAL_ROOM.rawValue == 3127)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_ENCRYPTED_FILE_CAN_ONLY_BE_RESTORED_IN_ORIGINAL_ROOM.description, "Encrypted files cannot be restored inside another than its original room.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_KEEPSHARELINKS_ONLY_WITH_OVERWRITE.rawValue == 3128)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_KEEPSHARELINKS_ONLY_WITH_OVERWRITE.description, "Keep share links is only allowed with resolution strategy 'overwrite'.")
+        
+        // Shares
+        
+        XCTAssert(DracoonApiCode.VALIDATION_DL_SHARE_CANNOT_CREATE_ON_ENCRYPTED_ROOM_FOLDER.rawValue == 3200)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_DL_SHARE_CANNOT_CREATE_ON_ENCRYPTED_ROOM_FOLDER.description, "A download share cannot be created on a encrypted room or folder.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_UL_SHARE_NAME_ALREADY_EXISTS.rawValue == 3201)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_UL_SHARE_NAME_ALREADY_EXISTS.description, "Upload share name already exists.")
+        
+        // Customer
+        
+        XCTAssert(DracoonApiCode.VALIDATION_PLACEHOLDER_CUSTOMER.rawValue == 3400)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_PLACEHOLDER_CUSTOMER.description, "Placeholder for customer validation errors")
+        
+        // Users
+        
+        XCTAssert(DracoonApiCode.VALIDATION_PLACEHOLDER_USERS.rawValue == 3500)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_PLACEHOLDER_USERS.description, "Placeholder for users validation errors")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_USER_HAS_NO_KEY_PAIR.rawValue == 3550)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_USER_HAS_NO_KEY_PAIR.description, "User has no encryption key pair.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_USER_KEY_PAIR_INVALID.rawValue == 3551)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_USER_KEY_PAIR_INVALID.description, "Invalid encryption key pair.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_USER_FILE_KEY_MISSING.rawValue == 3552)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_USER_FILE_KEY_MISSING.description, "File key has to be provided")
+        
+        // Groups
+        
+        XCTAssert(DracoonApiCode.VALIDATION_PLACEHOLDER_GROUPS.rawValue == 3600)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_PLACEHOLDER_GROUPS.description, "Placeholder for groups validation errors")
+        
+        // Other
+        
+        XCTAssert(DracoonApiCode.VALIDATION_PASSWORD_NOT_SECURE.rawValue == 3800)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_PASSWORD_NOT_SECURE.description, "Password is not secure.")
+        
+        XCTAssert(DracoonApiCode.VALIDATION_INVALID_EMAIL_ADDRESS.rawValue == 3801)
+        XCTAssertEqual(DracoonApiCode.VALIDATION_INVALID_EMAIL_ADDRESS.description, "Invalid email address.")
+        
     }
 }
